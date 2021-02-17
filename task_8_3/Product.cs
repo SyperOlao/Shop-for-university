df2c95f8a0eac5de8c87908142816129d986af84
Syper_olao <olay1234538@gmail.com>
2021-02-17 22:17:32 +0300

    Добавьте файлы проекта.

---
+++ task_8_3/Product.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task_8_3
+{
+    class Product
+    {
+        public string Name { set; get; }
+        public int MarketPrice { set; get; }
+        public string ProductionDate { set; get; }
+        public string ShelfLife { set; get; }
+        
+        public int WholesalePrice { set; get; }
+}
+}
---
+++ task_8_3/Store.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*Предусмотреть возможность
+выполнения следующих операций: реализация товара(партии товаров) с указанием
+даты совершения операции, подсчета выручки за указанный период времени,
+поступления новой партии товара.*/
+
+namespace task_8_3
+{
+    class Store
+    {
+        private List<Product> products = new List<Product>();
+        private Dictionary<int, int> dateOfOperations = new Dictionary<int, int>(); 
+
+        public void AddNewProducts(List<Product> products)
+        {
+
+            foreach (var item in products)
+            {
+                this.products.Add(item);
+            }
+        }
+
+    }
+}
