86afcbf8ecd64f491cc4186a28b51ede85dbd7cc
SyperOlao <olay1234538@gmail.com>
2021-03-30 23:31:03 +0300

    Добавлены ВАЖНЫЕ изменения во фронте

--- task_8_3/Form1.Designer.cs
+++ task_8_3/Form1.Designer.cs
@@ -46,13 +46,17 @@
             this.label7 = new System.Windows.Forms.Label();
             this.label8 = new System.Windows.Forms.Label();
             this.textBox7 = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(55, 383);
+            this.button1.Location = new System.Drawing.Point(41, 311);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(126, 55);
+            this.button1.Size = new System.Drawing.Size(94, 45);
             this.button1.TabIndex = 0;
             this.button1.Text = "Купить ";
             this.button1.UseVisualStyleBackColor = true;
@@ -60,71 +64,79 @@
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(22, 54);
+            this.textBox1.Location = new System.Drawing.Point(16, 44);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.textBox1.Multiline = true;
             this.textBox1.Name = "textBox1";
             this.textBox1.ReadOnly = true;
-            this.textBox1.Size = new System.Drawing.Size(235, 294);
+            this.textBox1.Size = new System.Drawing.Size(177, 240);
             this.textBox1.TabIndex = 1;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label1.Location = new System.Drawing.Point(30, 22);
+            this.label1.Location = new System.Drawing.Point(11, 2);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(203, 29);
+            this.label1.Size = new System.Drawing.Size(241, 24);
             this.label1.TabIndex = 2;
-            this.label1.Text = "Список товаров:";
+            this.label1.Text = "Список ПАРТИИ товаров:";
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(273, 54);
+            this.textBox2.Location = new System.Drawing.Point(205, 65);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.textBox2.Multiline = true;
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(192, 173);
+            this.textBox2.Size = new System.Drawing.Size(145, 141);
             this.textBox2.TabIndex = 3;
             // 
             // textBox3
             // 
-            this.textBox3.Location = new System.Drawing.Point(607, 33);
+            this.textBox3.Location = new System.Drawing.Point(455, 27);
+            this.textBox3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(181, 22);
+            this.textBox3.Size = new System.Drawing.Size(137, 20);
             this.textBox3.TabIndex = 4;
             this.textBox3.Text = "22.5.2020";
             // 
             // textBox4
             // 
-            this.textBox4.Location = new System.Drawing.Point(607, 102);
+            this.textBox4.Location = new System.Drawing.Point(455, 83);
+            this.textBox4.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(181, 22);
+            this.textBox4.Size = new System.Drawing.Size(137, 20);
             this.textBox4.TabIndex = 5;
             this.textBox4.Text = "7.6.2020";
             // 
             // textBox5
             // 
-            this.textBox5.Location = new System.Drawing.Point(551, 399);
+            this.textBox5.Location = new System.Drawing.Point(413, 324);
+            this.textBox5.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.textBox5.Multiline = true;
             this.textBox5.Name = "textBox5";
             this.textBox5.ReadOnly = true;
-            this.textBox5.Size = new System.Drawing.Size(208, 31);
+            this.textBox5.Size = new System.Drawing.Size(157, 26);
             this.textBox5.TabIndex = 6;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label2.Location = new System.Drawing.Point(257, 33);
+            this.label2.Location = new System.Drawing.Point(202, 23);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(270, 18);
+            this.label2.Size = new System.Drawing.Size(191, 15);
             this.label2.TabIndex = 7;
-            this.label2.Text = "Какие товары хотите купить/продать:";
+            this.label2.Text = "Какие  ПАРТИИ  товары хотите";
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(226, 383);
+            this.button2.Location = new System.Drawing.Point(170, 311);
+            this.button2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(126, 55);
+            this.button2.Size = new System.Drawing.Size(94, 45);
             this.button2.TabIndex = 8;
             this.button2.Text = "Продать";
             this.button2.UseVisualStyleBackColor = true;
@@ -132,9 +144,10 @@
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(391, 383);
+            this.button3.Location = new System.Drawing.Point(293, 311);
+            this.button3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(126, 55);
+            this.button3.Size = new System.Drawing.Size(94, 45);
             this.button3.TabIndex = 9;
             this.button3.Text = "Прибыль";
             this.button3.UseVisualStyleBackColor = true;
@@ -144,9 +157,10 @@
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label3.Location = new System.Drawing.Point(586, 9);
+            this.label3.Location = new System.Drawing.Point(440, 7);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(202, 18);
+            this.label3.Size = new System.Drawing.Size(168, 15);
             this.label3.TabIndex = 10;
             this.label3.Text = "Промежуток времени (нач):";
             // 
@@ -154,9 +168,10 @@
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label4.Location = new System.Drawing.Point(586, 81);
+            this.label4.Location = new System.Drawing.Point(440, 66);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(144, 18);
+            this.label4.Size = new System.Drawing.Size(120, 15);
             this.label4.TabIndex = 11;
             this.label4.Text = "Конец промежутка:";
             // 
@@ -164,17 +179,19 @@
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label5.Location = new System.Drawing.Point(548, 378);
+            this.label5.Location = new System.Drawing.Point(411, 307);
+            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(58, 18);
+            this.label5.Size = new System.Drawing.Size(46, 15);
             this.label5.TabIndex = 12;
             this.label5.Text = "Доход:";
             // 
             // textBox6
             // 
-            this.textBox6.Location = new System.Drawing.Point(273, 283);
+            this.textBox6.Location = new System.Drawing.Point(205, 230);
+            this.textBox6.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.textBox6.Name = "textBox6";
-            this.textBox6.Size = new System.Drawing.Size(192, 22);
+            this.textBox6.Size = new System.Drawing.Size(145, 20);
             this.textBox6.TabIndex = 13;
             this.textBox6.Text = "4.6.2020";
             // 
@@ -182,9 +199,10 @@
             // 
             this.label6.AutoSize = true;
             this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label6.Location = new System.Drawing.Point(270, 262);
+            this.label6.Location = new System.Drawing.Point(202, 213);
+            this.label6.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(116, 18);
+            this.label6.Size = new System.Drawing.Size(99, 15);
             this.label6.TabIndex = 14;
             this.label6.Text = "Дата операции:";
             // 
@@ -192,9 +210,10 @@
             // 
             this.label7.AutoSize = true;
             this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label7.Location = new System.Drawing.Point(270, 308);
+            this.label7.Location = new System.Drawing.Point(202, 250);
+            this.label7.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(169, 18);
+            this.label7.Size = new System.Drawing.Size(141, 15);
             this.label7.TabIndex = 15;
             this.label7.Text = "Формат даты (д.м.гггг)";
             // 
@@ -202,29 +221,69 @@
             // 
             this.label8.AutoSize = true;
             this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label8.Location = new System.Drawing.Point(548, 140);
+            this.label8.Location = new System.Drawing.Point(411, 114);
+            this.label8.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(93, 18);
+            this.label8.Size = new System.Drawing.Size(78, 15);
             this.label8.TabIndex = 16;
             this.label8.Text = "Инструкция:";
             // 
             // textBox7
             // 
-            this.textBox7.Location = new System.Drawing.Point(551, 161);
+            this.textBox7.Location = new System.Drawing.Point(413, 131);
+            this.textBox7.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.textBox7.Multiline = true;
             this.textBox7.Name = "textBox7";
             this.textBox7.ReadOnly = true;
-            this.textBox7.Size = new System.Drawing.Size(208, 126);
+            this.textBox7.Size = new System.Drawing.Size(157, 134);
             this.textBox7.TabIndex = 17;
             this.textBox7.Text = "Если хотите купить товар то напишите его (имя цену закупа цену рыночную), наприме" +
     "р: Лук 30 40. Продать (имя товара)! Например: Лук.     !!! УКАЖИТЕ ДАТУ ОПЕРАЦИИ" +
     "!!!";
             // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label9.ForeColor = System.Drawing.Color.Red;
+            this.label9.Location = new System.Drawing.Point(80, 2);
+            this.label9.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(86, 24);
+            this.label9.TabIndex = 18;
+            this.label9.Text = "ПАРТИЙ";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label10.Location = new System.Drawing.Point(202, 44);
+            this.label10.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(101, 15);
+            this.label10.TabIndex = 19;
+            this.label10.Text = "купить/продать:";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label11.ForeColor = System.Drawing.Color.Red;
+            this.label11.Location = new System.Drawing.Point(245, 23);
+            this.label11.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(56, 15);
+            this.label11.TabIndex = 20;
+            this.label11.Text = "ПАРТИИ";
+            // 
             // Shop
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(600, 366);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
             this.Controls.Add(this.textBox7);
             this.Controls.Add(this.label8);
             this.Controls.Add(this.label7);
@@ -243,6 +302,7 @@
             this.Controls.Add(this.label1);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Shop";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
@@ -271,6 +331,9 @@
         private System.Windows.Forms.Label label7;
         private System.Windows.Forms.Label label8;
         private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
     }
 }
 
