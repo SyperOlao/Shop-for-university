190fdb34d7f83e0c4baadc038d03b162422a4dbe
SyperOlao <olay1234538@gmail.com>
2021-04-18 23:44:44 +0300

    Изменения в продуктах

--- task_8_3/Store.cs
+++ task_8_3/Store.cs
@@ -1,7 +1,18 @@
 ﻿using System.Collections.Generic;
 using System.IO;
 
-
+/*Класс Store – набор объектов класса Product, экземпляры которого описывают
+товары, предлагаемые к продаже. В числе полей класса Product должны
+присутствовать уникальный код продукта, его категория и название, закупочная и
+розничная цена, единица измерения (кг, штука, упаковка). Класс Product должен
+включать метод вывода информации о конкретном продукте. Класс Store должен
+включать поле объема имеющихся оборотных средств. Методы класса Store должны
+обеспечивать выполнение операций по реализации товара с указанием даты
+совершения операции, подсчета выручки за указанный период времени,
+поступления повой партии товара, а также по формированию полного списка
+имеющихся товаров с указанием их объема В этом списке предусмотреть
+возможность группировки по товаров по категориям. Создать отдельные формы для
+экземпляров класса Store и класса Product.*/
 
 namespace task_8_3
 {
@@ -46,13 +57,14 @@ namespace task_8_3
             }
             return revenue;
         }
-        
+
         public Store(string path)
         {
             string[] file = File.ReadAllLines(path);
-            for (int i = 0; i < file.Length; i+=3)
+            for (int i = 0; i < file.Length; i += 3)
             {
-                products.Add(new Product(file[i], int.Parse(file[i+1]), int.Parse(file[i+2])));
+                // int Id, string Name, int MarketPrice, int WholesalePrice, string Category, int Quantity, string Unit
+                products.Add(new Product(int.Parse(file[i]), file[i + 1], int.Parse(file[i + 2]), int.Parse(file[i + 3]), file[i + 4], int.Parse(file[i + 5]), file[i + 6]));
             }
         }
 
